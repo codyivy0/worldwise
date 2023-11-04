b203dbea7bb68f81159e77dd653897633b6050fb
codyivy0 <codyivy0@gmail.com>
2023-11-04 15:26:53 -0500

    added protected route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { CitiesProvider } from "./context/CitiesContext";
 import { AuthProvider } from "./context/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const App = () => {
   return (
@@ -24,7 +25,14 @@ const App = () => {
               <Route path="product" element={<Product />} />
               <Route path="pricing" element={<Pricing />} />
               <Route path="login" element={<Login />} />
-              <Route path="app" element={<AppLayout />}>
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
                 <Route index element={<Navigate replace to="cities" />} />
                 <Route path="cities" element={<CityList />} />
                 <Route path="cities/:id" element={<City />} />
